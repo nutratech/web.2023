f6310aafe0784b495792454185e16e6f4cc5104d
Shane Jaroch <chown_tee@proton.me>
2023-04-09 19:04:04 -0400

    better binding

--- src/routes/calculators/+page.svelte
+++ src/routes/calculators/+page.svelte
@@ -5,11 +5,18 @@
 	// VARIABLES - One rep max
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	let ormWeightLifted = -1;
+	let ormWeightLifted;
+	let ormRepsPerformed;
+	let ormTableData = [[]];
 
-	export function updateTable1RM(e) {
-		console.info(`e.target=${JSON.stringify(e.target)}`);
-		console.info(`e.target.value=${e.target.value}`);
+	export function updateTable1RM() {
+		console.info(`ormWeightLifted=${ormWeightLifted}`);
+		console.info(`ormRepsPerformed=${ormRepsPerformed}`);
+		if (ormWeightLifted && ormRepsPerformed) {
+			ormTableData = [[0, 20, 20, 20]];
+		} else {
+			ormTableData = [[]];
+		}
 	}
 </script>
 
@@ -19,13 +26,25 @@
 	<h1>Calculators</h1>
 	<p><a href="/">Home page</a> - Click to go back</p>
 	<p>Calculators for various fitness metrics.</p>
-
+	<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+	<!--	One rep max-->
+	<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 	<h2>One-rep max</h2>
-	<form onsubmit="submitForm(this)">
+	<form>
 		<p>Weight lifted</p>
-		<input type="number" placeholder="Weight lifted, e.g. 85" on:input={updateTable1RM} />
+		<input
+			type="number"
+			placeholder="Weight lifted, e.g. 85"
+			bind:value={ormWeightLifted}
+			on:input={updateTable1RM}
+		/>
 		<p>Number of reps performed</p>
-		<input type="number" placeholder="Reps performed, e.g. 10" on:input={updateTable1RM} />
+		<input
+			type="number"
+			placeholder="Reps performed, e.g. 10"
+			bind:value={ormRepsPerformed}
+			on:input={updateTable1RM}
+		/>
 	</form>
 	<p>Predicted max weight (for <i>n</i> reps)</p>
 	<table>
@@ -37,7 +56,15 @@
 				<th>dos Remedios</th>
 			</tr>
 		</thead>
-		<tbody />
+		<tbody>
+			{#each Object.values(ormTableData) as row}
+				<tr>
+					{#each Object.values(row) as cell}
+						<td>{cell}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
 	</table>
 
 	<h2>Calorie expenditure (BMR)</h2>
