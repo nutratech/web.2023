b3efbfebf71d921ba947ffde6f5f20aab563a417
Shane Jaroch <chown_tee@proton.me>
2023-04-09 17:26:57 -0400

    start calculator form; add editorconfig

--- src/routes/calculators/+page.svelte
+++ src/routes/calculators/+page.svelte
@@ -2,11 +2,24 @@
 	import '@picocss/pico';
 </script>
 
-<title>Home Page | NutraTech</title>
+<title>Calculators | NutraTech</title>
 
 <main class="container">
 	<h1>Calculators</h1>
-	<p>Work in progress...</p>
-
 	<p><a href="/">Home page</a> - Click to go back</p>
+	<p>Calculators for various fitness metrics.</p>
+
+	<h2>One-rep max</h2>
+	<form>
+		<p>Weight lifted</p>
+		<input type="number" placeholder="Weight lifted, e.g. 85" />
+		<p>Number of reps performed</p>
+		<input type="number" placeholder="Reps performed, e.g. 10" />
+	</form>
+
+	<h2>Calorie expenditure (BMR)</h2>
+
+	<h2>Body fat</h2>
+
+	<h2>Lean body limits (young men)</h2>
 </main>
