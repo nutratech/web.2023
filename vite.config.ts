e75e2f48f4f92eb7ac936a363b41e07a226cb468
Shane Jaroch <chown_tee@proton.me>
2023-04-15 12:08:30 -0400

    legacy bundler/svelte is broken, see github issue:
        https://github.com/sveltejs/kit/issues/3247

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		legacy({
+			targets: ['defaults', 'not IE 11']
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
